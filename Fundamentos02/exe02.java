f5fd70012668f8552e7b716972dfde05f1dee6b3
Marcosan004 <128729650+Marcosan004@users.noreply.github.com>
2023-06-26 15:44:24 -0300

    Add files via upload

---
+++ Fundamentos02/exe02.java
@@ -0,0 +1,16 @@
+package Fundamentos02;
+
+public class exe02 {
+
+	public static void main(String[] args) {
+		
+		int cont = 1;
+		
+		while (cont < 101) {
+			if(cont % 2 == 1) {
+			System.out.println(cont);
+			}
+			cont++;
+		}
+	}
+}
---
+++ Fundamentos02/exe03.java
@@ -0,0 +1,17 @@
+package Fundamentos02;
+
+public class exe03 {
+
+	public static void main(String[] args) {
+		
+		int cont = 1;
+		
+		while(cont < 101) {
+			if(cont % 2 == 0) {
+				System.out.println(cont);
+			}
+			cont++;
+		}
+	}
+
+}
