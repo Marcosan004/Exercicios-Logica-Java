f2cf6fd21c7184d3ec8a2f96a34a25b382ebd36b
Marcosan004 <128729650+Marcosan004@users.noreply.github.com>
2023-07-03 16:32:03 -0300

    Add files via upload

---
+++ Fundamentos02/exe05.java
@@ -0,0 +1,19 @@
+package Fundamentos02;
+
+import java.util.Scanner;
+
+public class exe05 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		for(int i = 0;i < 101; i++) {
+			System.out.println(i);
+			if (i == 19) {
+				break;
+			}	
+		}
+		
+	}
+
+}
